8cb95e6af22d582a10b4bc95c920f217266b469d
bcelikag@gmail.com <43502101+bombolini@users.noreply.github.com>
2022-05-13 11:52:59 +0300

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,12 @@
+prename(){
+  if [ "x$1" = x-v ]; then v=$1; shift; else v=; fi
+  subst=$1; shift
+  for arg
+  do
+    dest=$(printf "%s\n" "$arg" | sed "$subst") || return
+    [ "$arg" != "$dest" ] && mv $v "$arg" "$dest"
+  done
+}
+#example:
+prename -v "s|[[]|\.|g" *
+prename -v "s|[]]|\.|g" *
